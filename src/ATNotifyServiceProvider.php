85b3c8b110339eb2dcd794290aca0289069e8119
Mauko Maunde <maukoese@gmail.com>
2019-12-22 22:19:51 +0300

    Update and rename ServiceNameServiceProvider.php to ATNotifyServiceProvider.php

--- src/ATNotifyServiceProvider.php
+++ src/ATNotifyServiceProvider.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace NotificationChannels\:channel_namespace;
+namespace NotificationChannels\AT;
 
 use Illuminate\Support\ServiceProvider;
 
-class :service_nameServiceProvider extends ServiceProvider
+class ATNotifyServiceProvider extends ServiceProvider
 {
     /**
      * Bootstrap the application services.
